ecfbecf407b793e9a6d455f060ccde7d2ab16f38
Andrey Shuster <novus42@gmail.com>
2020-06-07 22:30:01 +0300

    Add ansible playbook to install nginx and display static page Add ingress port 80 to security group, to allow access to nginx site

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,20 +3,29 @@ resource "aws_instance" "test_instance" {
     instance_type = "t2.micro"
     key_name = "Lenovo-laptop"
     security_groups = [
-        aws_security_group.ssh_access.name
+      aws_security_group.ssh_access.name
     ]
 
     tags = {
-        Name = "sandboxy"
+      Name = "sandboxy"
     }
 }
 
 resource "aws_security_group" "ssh_access" {
   name = "test_sg"
 
+  # ssh
   ingress {
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # http
+  ingress {
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
